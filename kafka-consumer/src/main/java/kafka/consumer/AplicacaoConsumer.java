dfb4c4a5a528cf7fa979341b84b7cef53122e741
tiago.lima <tiago.lima@compasso.com.br>
2022-03-31 10:54:49 -0300

    Implementando projeto com mensageria kafka

---
+++ kafka-consumer/src/main/java/kafka/consumer/AplicacaoConsumer.java
@@ -0,0 +1,19 @@
+package kafka.consumer;
+
+import kafka.consumer.eventos.ConsumidorEvento;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class AplicacaoConsumer {
+
+    public static void main(String[] args) {
+        AplicacaoConsumer aplicacao = new AplicacaoConsumer();
+        aplicacao.iniciar();
+    }
+
+    private void iniciar() {
+        log.info("Iniciando a aplicação");
+        ConsumidorEvento consumidor = new ConsumidorEvento();
+        consumidor.executar();
+    }
+}
---
+++ kafka-producer/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+h
+8src/main/java/kafka/producer/eventos/ProdutorEvento.java,2\f\2f69226e746b609663fa7d69275bb4c66301fdb9
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+c
+3src/main/java/kafka/producer/AplicacaoProducer.java,1\7\1741fe7cdbe59abfcd164989204d0d48aee7891e
+j
+:src/main/java/kafka/consumer/eventos/ConsumidorEvento.java,f\f\ffab7552588ee4a335f376547da01f3a98cd9b01
+c
+3src/main/java/kafka/consumer/AplicacaoConsumer.java,4\d\4dbeb72a196119f219070ec7c2711f05eb323f91
